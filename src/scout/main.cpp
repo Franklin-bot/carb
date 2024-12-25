b443e06e10142058a678aeeb22a0c9c35c3e1260
Milan Shah <milanmshah04@gmail.com>
2024-12-24 19:18:11 -0500

    di a rebase?

--- src/scout/main.cpp
+++ src/scout/main.cpp
@@ -5,10 +5,16 @@
 
 int main(){
 
+<<<<<<< HEAD
     const std::vector<std::string> products = {"MATIC"};
+=======
+    // const std::vector<std::string> products = {"ETH"};
+    const std::vector<std::string> products = {"MATIC"};
+    // const std::vector<std::string> channels = {"ticker", "level2", "heartbeat"};
+>>>>>>> 86e50b9 (python analysis)
     const std::vector<std::string> channels = {"ticker", "book"};
-    std::unique_ptr<Scout> test = std::make_unique<Scout>(10, products);
-    test->listenAndNotJudge(10);
+    std::unique_ptr<Scout> test = std::make_unique<Scout>(60*5, products);
+    test->listenAndNotJudge(60*5);
 
     return 0;
 
