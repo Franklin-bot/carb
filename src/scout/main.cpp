4c8163a2c3f6eb630db755e9f732c64ae94edf8c
Milan Shah <milanmshah04@gmail.com>
2024-12-24 19:20:26 -0500

    fixed more bugs

--- src/scout/main.cpp
+++ src/scout/main.cpp
@@ -5,13 +5,7 @@
 
 int main(){
 
-<<<<<<< HEAD
     const std::vector<std::string> products = {"MATIC"};
-=======
-    // const std::vector<std::string> products = {"ETH"};
-    const std::vector<std::string> products = {"MATIC"};
-    // const std::vector<std::string> channels = {"ticker", "level2", "heartbeat"};
->>>>>>> 86e50b9 (python analysis)
     const std::vector<std::string> channels = {"ticker", "book"};
     std::unique_ptr<Scout> test = std::make_unique<Scout>(60*5, products);
     test->listenAndNotJudge(60*5);
